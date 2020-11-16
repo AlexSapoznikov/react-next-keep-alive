3b61535a7f16ab40e5003dbda9f132579bbed0d5
Alex Sapoznikov <alex@nodeswat.com>
2020-11-16 12:20:38 +0200

    Update Component type

--- example/components/component.tsx
+++ example/components/component.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, Fragment, ComponentType } from 'react';
+import React, { ReactElement, Fragment } from 'react';
 import { NextComponentType } from 'next'; // eslint-disable-line import/no-extraneous-dependencies
 import { KeepAliveName, KeepAliveOptsProps } from './provider';
 
@@ -11,7 +11,11 @@ const defaultOpts: KeepAliveOptsProps = {
   applyNewProps: false
 };
 
-const withKeepAlive = (Component: ComponentType & NextComponentType, name: KeepAliveName, opts: KeepAliveOptsProps = defaultOpts) => {
+const withKeepAlive = (
+  Component: NextComponentType & ((props?: Object) => JSX.Element),
+  name: KeepAliveName,
+  opts: KeepAliveOptsProps = defaultOpts
+) => {
   const KeepAlive = (props: KeepAliveProps) => (
     // eslint-disable-next-line react/jsx-fragments
     <Fragment>
